6bbb8324324420a4b0680996b50f1c8ff5382d63
rahul95138 <rahulchakilam2@gmail.com>
2024-02-15 13:41:56 -0500

    added code

--- App.js
+++ App.js
@@ -1,20 +1,86 @@
+import axios from 'axios';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
+import * as ScreenOrientation from 'expo-screen-orientation';
+
+
 
 export default function App() {
+
+  // ScreenOrientation.addOrientationChangeListener()
+
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    const handleOrientationChange = ({ orientationInfo }) => {
+      console.log('Orientation changed:', orientationInfo.orientation);
+      // Perform actions based on the new orientation
+    };
+
+    // Add the orientation change listener
+    const subscription = ScreenOrientation.addOrientationChangeListener(handleOrientationChange);
+
+    // Cleanup: Remove the listener when the component unmounts
+    return () => {
+      subscription.remove();
+    };
+  }, []);
+
+
+  const fetchData = async () =>{
+    try{
+      const countryData = await axios.get("https://gist.githubusercontent.com/peymano-wmt/32dcb892b06648910ddd40406e37fdab/raw/db25946fd77c5873b0303b858e861ce724e0dcd0/countries.json")
+      if(countryData.status === 200){
+        const {data} = countryData
+        setData([...data])
+      }
+    }catch(err){
+      console.log("err", error)
+    }
+  }
+
+  useEffect(()=>{
+    fetchData()
+  }, [])
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.table}>
+        <View style={styles.row}>
+          <Text style={styles.cell}>Name</Text>
+          <Text style={styles.cell}>Capital</Text>
+        </View>
+        <ScrollView>
+        {data.map(({name,region,code, capital}) => (<View style={styles.row}>
+          <Text style={styles.cell}>{name} || {region} || {code} </Text>
+          <Text style={styles.cell}>{capital}</Text>
+        </View>))}
+        </ScrollView>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container:{
+    height:"91%"
+  },
+  table: {
+    borderWidth: 1,
+    borderColor: '#000',
+    margin: 10,
+    marginTop:70,
+    marginHorizontal:30
+  },
+  row: {
+    flexDirection: 'row',
+    borderBottomWidth: 1,
+    borderColor: '#000',
+  },
+  cell: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 10,
+    textAlign: 'center',
   },
 });
